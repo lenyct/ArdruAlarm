0fc4be3f9a0a6fcc054169301544d3bbc9008069
Paulleonard <Paul.leonard88@hotmail.com>
2017-04-20 19:39:13 +0100

    Alarm component

---
+++ app/src/main/java/com/adrualarm/ardrualarm/AlarmReceiver.java
@@ -0,0 +1,18 @@
+package com.adrualarm.ardrualarm;
+
+import android.content.Context;
+import android.content.Intent;
+import android.media.Ringtone;
+import android.media.RingtoneManager;
+import android.net.Uri;
+import android.support.v4.content.WakefulBroadcastReceiver;
+
+public class AlarmReceiver extends WakefulBroadcastReceiver {
+    @Override
+    public void onReceive(final Context context, Intent intent) {
+        MainActivity.getTextView2().setText("It's Mo fucker Coffee Time Biatch");
+        Uri uri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_ALARM);
+        Ringtone ringtone = RingtoneManager.getRingtone(context, uri);
+        ringtone.play();
+    }
+}
